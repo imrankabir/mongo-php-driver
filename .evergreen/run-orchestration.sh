0918d7e8931d7082175d02cc1bffaa86b9bedd4e
Derick Rethans <github@derickrethans.nl>
2018-11-20 10:13:14 +0000

    Use our own configs

---
+++ .evergreen/run-orchestration.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+set -o xtrace   # Write all commands first to stderr
+set -o errexit  # Exit the script with error if any of the commands fail
+
+
+AUTH=${AUTH:-noauth}
+SSL=${SSL:-nossl}
+TOPOLOGY=${TOPOLOGY:-server}
+STORAGE_ENGINE=${STORAGE_ENGINE}
+MONGODB_VERSION=${MONGODB_VERSION:-latest}
+
+DL_START=$(date +%s)
+DIR=$(dirname $0)
+# Functions to fetch MongoDB binaries
+. ${DRIVER_TOOLS}/.evergreen/download-mongodb.sh
+
+get_distro
+if [ -z "$MONGODB_DOWNLOAD_URL" ]; then
+    get_mongodb_download_url_for "$DISTRO" "$MONGODB_VERSION"
+else
+  # Even though we have the MONGODB_DOWNLOAD_URL, we still call this to get the proper EXTRACT variable
+  get_mongodb_download_url_for "$DISTRO"
+fi
+download_and_extract "$MONGODB_DOWNLOAD_URL" "$EXTRACT"
+
+DL_END=$(date +%s)
+MO_START=$(date +%s)
+
+export ORCHESTRATION_FILE="$PROJECT_DIRECTORY/scripts/presets/travis/${TOPOLOGY}"
+export ORCHESTRATION_URL="http://localhost:8889/v1/${TOPOLOGY}s"
+
+# Start mongo-orchestration
+sh ${DRIVER_TOOLS}/start-orchestration.sh "$MONGO_ORCHESTRATION_HOME"
+
+pwd
+curl --silent --show-error --data @"$ORCHESTRATION_FILE" "$ORCHESTRATION_URL" --max-time 600 --fail -o tmp.json
+cat tmp.json
+URI=$(python -c 'import sys, json; j=json.load(open("tmp.json")); print(j["mongodb_auth_uri" if "mongodb_auth_uri" in j else "mongodb_uri"])' | tr -d '\r')
+echo 'MONGODB_URI: "'$URI'"' > mo-expansion.yml
+echo "Cluster URI: $URI"
+
+MO_END=$(date +%s)
+MO_ELAPSED=$(expr $MO_END - $MO_START)
+DL_ELAPSED=$(expr $DL_END - $DL_START)
+cat <<EOT >> $DRIVERS_TOOLS/results.json
+{"results": [
+  {
+    "status": "PASS",
+    "test_file": "Orchestration",
+    "start": $MO_START,
+    "end": $MO_END,
+    "elapsed": $MO_ELAPSED
+  },
+  {
+    "status": "PASS",
+    "test_file": "Download MongoDB",
+    "start": $DL_START,
+    "end": $DL_END,
+    "elapsed": $DL_ELAPSED
+  }
+]}
+
+EOT
